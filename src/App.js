dd32fcee2b1bb200dd901ba7ee8da23f4cf08d57
ajayg415 <ajay.g415@gmail.com>
2020-08-16 22:27:34 +0530

    added logout logic and moved contolrs to app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 import AppHeader from './components/app-header/AppHeader';
 import AppMessage from './components/app-message/AppMessage'
 import AppLogin from './components/app-login/AppLogin'
+import AppWorkflows from './components/app-workflow/AppWorkflows'
+import { userLogout } from './store/actions'
 
 import './App.css';
 
-const App = ({ isAuth }) => {
-  console.log('isAuth :', isAuth)
+const App = ({ isAuth, dispatchLogOut }) => {
   return (
     <Router>
       <div className="automate-app container">
-        <AppHeader /> 
+        <AppHeader dispatchLogOut={dispatchLogOut} isAuth={isAuth} /> 
         <AppMessage />
         <Switch>
-          <Route path="/" exact component={AppLogin}/>
+          <Route path="/" exact component={AppLogin} />
+          <Route path="/workflows" component={AppWorkflows} render={() => {
+            return !isAuth ? <Redirect to="/" /> : ''
+          }} />
         </Switch>
       </div>
     </Router>
@@ -27,4 +31,8 @@ const mapStateToProps = state => ({
   isAuth: state.appReducer.isAuth
 })
 
-export default connect(mapStateToProps, null)(App);
+const mapDispatchToProps = dispatch => ({
+  dispatchLogOut: () => dispatch(userLogout())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/app-header/AppHeader.js
+++ src/components/app-header/AppHeader.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FcFlowChart } from "react-icons/fc";
+import { withRouter } from 'react-router-dom';
 
 import './AppHeader.css'
 
-const AppHeader = () => {
+const AppHeader = ({ dispatchLogOut, isAuth, history }) => {
+
+  useEffect(() => {
+    if(!isAuth) {
+      history.push('/')
+    }
+  }, [isAuth])
+
   return (
     <div className="bg-pink-700 flex justify-between m-1 px-1 py-5">
       <div className="w-1/2">
         <FcFlowChart className="chart-icon"/> 
         <div className="inline-block mx-3 text-white company-name">AUTOMATE FLOW APP</div>
       </div>
+      { isAuth &&
+        <div className="w-1/2">
+          <button 
+            onClick={() => dispatchLogOut()}
+            className="bg-white float-right font-semibold hover:bg-gray-200 mr-4 px-4 py-2 rounded" 
+            type="button">
+            Sign Out
+          </button>
+        </div>
+      }
     </div>
   )
 }
 
-export default AppHeader;
+export default withRouter(AppHeader);
