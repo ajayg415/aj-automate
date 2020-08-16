b2dfd10a2ad3def0fad0527a81cd6478e7e41042
ajayg415 <ajay.g415@gmail.com>
2020-08-16 13:26:39 +0530

    added static global message

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 
 import AppHeader from './components/app-header/AppHeader';
+import AppMessage from './components/app-message/AppMessage'
 import AppLogin from './components/app-login/AppLogin'
 
 function App() {
   return (
     <div className="automate-app container">
       <AppHeader /> 
+      <AppMessage />
       <AppLogin />
     </div>
   );
