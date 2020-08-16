13f9b0392dd3d323376ba2ccb6d318269b819a92
ajayg415 <ajay.g415@gmail.com>
2020-08-16 15:13:03 +0530

    linked app to redux store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
 import { IconContext } from "react-icons";
 
+import store from './store/sagas/store'
 import './assets/main.css';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <IconContext.Provider value={{ className: "react-icons" }}>
-    <App />
-  </IconContext.Provider>,
+  <Provider store={store}>
+    <IconContext.Provider value={{ className: "react-icons" }}>
+      <App />
+    </IconContext.Provider>
+  </Provider>,
   document.getElementById('root')
 );
 
