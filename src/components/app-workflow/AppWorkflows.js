4c4f10c127f3467a1a272e6477289c49b014c091
ajayg415 <ajay.g415@gmail.com>
2020-08-17 13:18:13 +0530

    added filter logic

--- src/components/app-workflow/AppWorkflows.js
+++ src/components/app-workflow/AppWorkflows.js
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 
 import WorkflowHeader from './WorkflowHeader';
 import WorkflowTile from './WorkflowTile'
 
 const AppWorkflows = ({ workflows }) => {
+  const [search, setSearch] = useState('')
+  const [filter, setFilter] = useState('-1')
+  const [cards, setCards] = useState([...workflows])
+
+  useEffect(() => {
+    setCards([...workflows])
+  },[workflows])
+
+  console.log('filter :', filter);
+  useEffect(() => {
+    let flows = []
+    if(filter == 'all' || filter == '-1'){
+      flows = [...workflows]
+    }else{
+      flows = workflows.filter(workflow => workflow.status === filter)
+    }
+
+    setCards(flows.filter(workflow =>{
+      return workflow.name.toLowerCase().indexOf(search.toLowerCase()) === 0
+    }))
+  },[search, filter])
+
   return (
     <div>
-      <WorkflowHeader />
-      
-      <div className="gap-4 grid grid-cols-3 grid-rows-3 m-5 md:grid-cols-3 sm:grid-cols-2 xl:grid-cols-4">
-        {workflows.map(workflow => {
-          return <WorkflowTile key={workflow.id} workflow={workflow} />
-        })}
-      </div>
+      <WorkflowHeader search={search} setSearch={setSearch} filter={filter} setFilter={setFilter}/>
+      {cards.length > 0 ?
+        <div className="gap-4 grid grid-cols-3 grid-rows-3 m-5 md:grid-cols-3 sm:grid-cols-2 xl:grid-cols-4">
+          {cards.map(workflow => {
+            return <WorkflowTile key={workflow.id} workflow={workflow} />
+          })}
+        </div>
+      :
+        <div className="flex font-semibold justify-center mt-56">
+          <span>No Workflows to show. Please add new workflow using 'Create Workflow' button</span>
+        </div>
+      }
     </div>
   )
 }
--- src/components/app-workflow/WorkflowHeader.js
+++ src/components/app-workflow/WorkflowHeader.js
@@ -6,19 +6,19 @@ import { IoMdAdd } from "react-icons/io";
 
 import './workflow.css'
 
-const WorkflowHeader = () => {
+const WorkflowHeader = ({ search, setSearch, filter, setFilter }) => {
   return (
     <div className="border-b-2 border-gray-500 flex justify-between py-5 workflow">
 
       <div className="flex justify-between">
         <div className="border flex ml-10 w-full search-box control">
           <BsSearch className="mr-3 ml-1 workflow-icons relative" />
-          <input type="text" className="focus:outline-none w-full" />
+          <input type="text" className="focus:outline-none w-full" value={search} onChange={e => setSearch(e.target.value)}/>
         </div>
         
         <div className="border border-gray-500 flex ml-6 control">
           <FaFilter className="mr-3 ml-1 workflow-icons relative" />
-          <select className="appearance-none focus:outline-none">
+          <select className="appearance-none focus:outline-none" value={filter} onChange={e=> setFilter(e.target.value)}>
             <option value="-1">Filter</option>
             <option value="all">All</option>
             <option value="completed">Completed</option>
